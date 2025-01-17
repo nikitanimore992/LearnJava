d995c5b4b0e57ba5b9fce2b425eaab8e39960d8d
nikita nimore <992nikitanimore@gmail.com>
2025-01-17 18:51:38 +0530

    complete this program

--- PatternQ5.java
+++ PatternQ5.java
@@ -1,5 +1,21 @@
 
 
 class PatternQ5{
+
+    public static void main(String[] args) {
+        int m =5;
+        int n=5;
     
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+      
+    }
 }
