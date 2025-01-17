23abad7dae56d4cf1c3b698512fff25d58ff33a3
nikita nimore <992nikitanimore@gmail.com>
2025-01-17 18:26:48 +0530

    create a new java file PatternQ5

--- pattarnQ4.java
+++ pattarnQ4.java
@@ -1,5 +1,16 @@
 
 
 class pattarnQ4{
-    
+
+    public static void main(String[] args) {
+        int m = 10;
+     for(int i=m; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.err.print("*");
+        }
+        System.err.println("");
+     }  
+      
+    }
+
 }
