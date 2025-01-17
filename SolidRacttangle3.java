c9b49862a68566e14dd236f74f27f2e438124a3e
nikita nimore <992nikitanimore@gmail.com>
2025-01-17 18:16:33 +0530

    complete this is program

--- SolidRacttangle3.java
+++ SolidRacttangle3.java
@@ -1,5 +1,20 @@
 
 
 class SolidRacttangle3{
-    
+   public static void main(String[] args) {
+    int n =5;
+    int m = 4;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            if(i==1 || j==1 || i==m || j==n){
+                System.out.print("*");
+            }else{
+
+                System.out.print(" ");
+            }
+        }
+        System.out.println("");
+    }
+   }
+
 }
